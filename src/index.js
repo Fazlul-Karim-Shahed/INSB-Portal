a880ada2dedb40422e3f4eaaa95001263b13b9db
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2022-11-10 03:01:40 +0600

    Routing add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    // <Provider store={''}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    // </Provider>
+);

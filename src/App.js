8584ed385fb634285aac2ea36563cb74e6e67f09
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2022-11-09 21:59:00 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainComponents from './Components/MainComponents'
+
+export default function App() {
+  return (
+    <div>
+      <MainComponents />
+    </div>
+  )
+}
